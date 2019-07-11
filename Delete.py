3e90e36da968265b340158118c423e3e6646d895
ExpertSeahorse <50875949+ExpertSeahorse@users.noreply.github.com>
2019-07-11 09:51:43 -0400

    Added quit function

--- Delete.py
+++ Delete.py
@@ -9,14 +9,15 @@ class Driver:
         self.label = tk.Label(self.master, text="Are you sure you want to delete?").pack()
         self.button = tk.Button(self.master, text="Delete", command=self.recursive_delete).pack()
         self.default_font = tkFont.nametofont("TkDefaultFont")
-        self.default_font.configure(size=20)
+        self.default_font.configure(size=18)
 
     # noinspection PyAttributeOutsideInit
     def recursive_delete(self):
-        self.app = self.Stats(tk.Toplevel(self.master))
+        self.app = self.NewWindow(tk.Toplevel(self.master))
 
-    class Stats:
+    class NewWindow:
         def __init__(self, master):
+            self.master = master
             with open('Delete_Strings.txt', 'r') as fin:
                 self.file = fin.read()
                 self.file_arr = self.file.split('\n')
@@ -25,8 +26,17 @@ class Driver:
 
             self.cont = tk.Button(master, text="Yes", command=app.recursive_delete)
             self.cont.grid(row=1, column=0)
-            self.stop = tk.Button(master, text="No", command=master.quit)
+            self.stop = tk.Button(master, text="No", command=self.quit)
             self.stop.grid(row=1, column=1)
+
+        def quit(self):
+            self.app = self.QuitWindow(tk.Toplevel(self.master))
+
+        class QuitWindow:
+            def __init__(self, master):
+                self.label = tk.Label(master, text="Oh ok")
+                self.label.after(2000, master.quit)
+                self.label.pack()
 
 
 if __name__ == '__main__':
